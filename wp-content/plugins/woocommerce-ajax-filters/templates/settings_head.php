a71dd6c3f3d53a113e8592c0434056fa4fa46b6b
Ryan Woo <rjwoo@sfu.ca>
2017-06-26 14:27:06 -0400

    Adding briancaicco@gmail.com

--- wp-content/plugins/woocommerce-ajax-filters/templates/settings_head.php
+++
@@ -1,9 +0,0 @@
-<div class="wrap">
-<?php
-$text = '<h1>%plugin_name% by BeRocket</h1>
-<div>%desc%</div>';
-$text = str_replace('%plugin_name%', $plugin_info['Name'], $text);
-$text = str_replace('%desc%', $plugin_info['Description'], $text);
-echo $text;
-?>
-</div>
--- wp-content/plugins/woocommerce-ajax-filters/templates/widget_update_button.php
+++
@@ -1,3 +0,0 @@
-<div class="berocket_aapf_widget">
-    <input type="button" value="<?php echo $title ?>" class="berocket_aapf_widget_update_button" />
-</div>
