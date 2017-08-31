3940471ac071c8cd74e05e0ad26c4dd5be55cfe8
Brian Caicco <briancaicco@gmail.com>
2017-08-31 14:16:15 -0400

    hotfix

--- wp-content/themes/sos-chapter-child/page-templates/homepage.php
+++ wp-content/themes/sos-chapter-child/page-templates/homepage.php
@@ -31,7 +31,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<div class="row no-gutters">
 		<div class="col-12 col-md-6 d-block align-middle py-6 px-4 py-lg-7 px-lg-6">
 				<h3>They know what you need to know...<br/>and what you don't.</h3>
-				<p class="lead">Learn from an A+ student who' has already aced your course.</p>
+				<p class="lead">Learn from an A+ student who has already aced your course.</p>
 				<a href="/shop" class="btn btn-outline-primary">Find a Session</a>
 		</div>
 			<div class="col-12 col-md-6 img-block-1 py-8 "></div>
