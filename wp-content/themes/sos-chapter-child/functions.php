fc296d317dfce714c9f7affcf77655f4608324e4
Brian Caicco <briancaicco@gmail.com>
2017-10-12 10:35:05 -0400

    hotfixes

--- wp-content/themes/sos-chapter-child/functions.php
+++ wp-content/themes/sos-chapter-child/functions.php
@@ -231,6 +231,8 @@ add_filter( 'woocommerce_page_title', 'woo_shop_page_title');
 function woo_shop_page_title( $page_title ) {
   if( 'Shop' == $page_title) {
     return "Sessions";
+ } else {
+    return $page_title;
  }
 }
 
