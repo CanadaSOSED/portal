3a39863b9b3dca060c6024171c4725fdff90ddf2
Ryan Woo <rjwoo@sfu.ca>
2017-11-30 19:09:11 -0500

    Altered the trip choice on trip-select template

--- wp-content/themes/sos/page-templates/trip-select.php
+++ wp-content/themes/sos/page-templates/trip-select.php
@@ -15,9 +15,14 @@ get_header(); ?>
       <h1 class="h1 animated fadeInDown" >Join us on one of our Outreach Trips!</h1>
       <div class="lead animated fadeIn">
         <p>We focus our work in communities across 8 different countries in Latin America. There is a huge need for projects within the region which means we can make a genuine and lasting impact.</p>
-      </div>
       <!-- <p class="animated fadeIn"></p> -->
+      <select id="chapters-list" class="custom-select" onchange="document.location.href=this.options[this.selectedIndex].value;">
+      <option> Select Your Campus </option>
+          <?php sos_chapters_list_option_box(); ?>
+      </select>
+      
       <a class="btn btn-primary btn-lg mx-auto my-3 d-block animated fadeIn" style="width: 200px;" href="<?php bloginfo('site_url'); ?>/trips" role="button">Find a Trip</a>
+    </div>
     </div>
   </div>
 </div>
