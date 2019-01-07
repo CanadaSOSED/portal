e528565c40095a166998a51cb10c3e8977211465
ismara alvim <ismaraalvim@gmail.com>
2019-01-07 20:27:08 -0200

    New faq page - fixing some issues

--- wp-content/themes/sos-knowledge-base-child/page-templates/homepage.php
+++ wp-content/themes/sos-knowledge-base-child/page-templates/homepage.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Right Sidebar Layout
+ * Template Name: Home Page - Knowledge Base
  *
  * This template can be used to override the default template and sidebar setup
  *
@@ -108,3 +108,86 @@ $container = get_theme_mod( 'understrap_container_type' );
 </div><!-- Wrapper end -->
 
 <?php get_footer(); ?>
+
+
+
+<!-- ***************************************************************************
+2018-01-07 - ismara - old PAGE for faq homepage
+********************************************************************************
+<?php
+/**
+ * Template Name: Home Page old - Knowledge Base
+ *
+ * This template can be used to override the default template and sidebar setup
+ *
+ * @package sos-knowledge-base
+ */
+/*
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="wrapper" id="wrapper-search">
+	<div class="home-search row justify-content-sm-center">
+		<div class="col-sm-7">
+			<!-- Display Custom Search box -->
+			<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+				<label class="assistive-text" for="s">Search</label>
+				<div class="form-group">
+					<input class="field form-control" id="s" name="s" type="text" placeholder="Search …">
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<div class="wrapper" id="page-wrapper">
+
+	<div class="<?php echo esc_html( $container ); ?>" id="content">
+
+		<div class="row site-main" id="main" role="main">
+
+			<div class="col-md-8">
+
+				<div class="row">
+					<div class="col-sm-12">
+						<h3>Help Topics</h3>
+						<hr/>
+						<br/>
+					</div>
+				</div>
+
+				<div class="row">
+
+					<div class="col-md-12">
+						<?php
+						get_sidebar( 'home_left' );
+						?>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<?php
+				get_sidebar( 'home_right' );
+				?>
+			</div>
+
+
+		</div>
+
+	</main><!-- #main -->
+
+</div><!-- #primary -->
+
+
+</div><!-- .row -->
+
+</div><!-- Container end -->
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); */?>
+
+-->
